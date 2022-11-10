5860b2208499bf3c9c5286b4f9f6cca73f17476e
Ricardo Pérez <ricpelo@gmail.com>
2022-11-10 21:01:30 +0100

    Flowbite

--- public/admin/index.php
+++ public/admin/index.php
@@ -17,22 +17,30 @@
     $sent = $pdo->query("SELECT * FROM articulos ORDER BY codigo");
     ?>
     <div class="container mx-auto">
-        <table class="mx-auto mt-4">
-            <thead>
-                <th>Código</th>
-                <th>Descripción</th>
-                <th>Precio</th>
-            </thead>
-            <tbody>
-                <?php foreach ($sent as $fila): ?>
-                    <tr>
-                        <td><?= hh($fila['codigo']) ?></td>
-                        <td><?= hh($fila['descripcion']) ?></td>
-                        <td><?= hh($fila['precio']) ?></td>
-                    </tr>
-                <?php endforeach ?>
-            </tbody>
-        </table>
+        <div class="overflow-x-auto relative mt-4">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <th scope="col" class="py-3 px-6">Código</th>
+                    <th scope="col" class="py-3 px-6">Descripción</th>
+                    <th scope="col" class="py-3 px-6">Precio</th>
+                    <th scope="col" class="py-3 px-6 text-center">Acciones</th>
+                </thead>
+                <tbody>
+                    <?php foreach ($sent as $fila): ?>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <td class="py-4 px-6"><?= hh($fila['codigo']) ?></td>
+                            <td class="py-4 px-6"><?= hh($fila['descripcion']) ?></td>
+                            <td class="py-4 px-6"><?= hh($fila['precio']) ?></td>
+                            <td class="py-4 px-6 text-center">
+                                <a href="#" class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900">Editar</a>
+                                <a href="#" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Borrar</a>
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
     </div>
+    <script src="/js/flowbite.js"></script>
 </body>
 </html>
