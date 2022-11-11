80cc0f66c0f6dcab690ffbc9fd3d27439478840a
Ricardo Pérez <ricpelo@gmail.com>
2022-11-11 18:21:17 +0100

    Realiza correctamente el borrado

--- public/admin/borrar.php
+++ public/admin/borrar.php
@@ -4,6 +4,23 @@ session_start();
 require '../../src/admin-auxiliar.php';
 require '../../src/auxiliar.php';
 
+$id = obtener_post('id');
+
+// if (!comprobar_csrf()) {
+//     return volver_admin();
+// }
+
+if (!isset($id)) {
+    return volver_admin();
+}
+
+// TODO: Validar id
+// Comprobar si el departamento tiene empleados
+
+$pdo = conectar();
+$sent = $pdo->prepare("DELETE FROM articulos WHERE id = :id");
+$sent->execute([':id' => $id]);
 
 $_SESSION['exito'] = 'El artículo se ha borrado correctamente.';
-return volver_admin();
+
+volver_admin();
